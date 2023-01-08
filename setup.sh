64b188991e28bb84c37c5cd4b33a3c4a62a2725e
blairwday <blair@blairday.com>
2023-01-08 10:05:58 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 set -e
 
-function setTimezone() {
-    local timezone=${1}
-    echo "${1}" | sudo tee /etc/timezone
-    sudo ln -fs "/usr/share/zoneinfo/${timezone}" /etc/localtime
-    sudo dpkg-reconfigure -f noninteractive tzdata
-}
+local timezone="America/Chicago"
+echo "${timezone}" | sudo tee /etc/timezone
+sudo ln -fs "/usr/share/zoneinfo/${timezone}" /etc/localtime
+sudo dpkg-reconfigure -f noninteractive tzdata
 
-
-read -r timezone
-if [ -z "${timezone}" ]; then
-    timezone="America/Chicago"
-fi
-setTimezone "${timezone}"
 echo "Timezone is set to $(cat /etc/timezone)" >&3
 
 

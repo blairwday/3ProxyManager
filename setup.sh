8a2dacb5ddfe8fa1014a85f326b5a6234af8d030
blairwday <blair@blairday.com>
2023-01-08 10:02:09 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 set -e
 
-function getCurrentDir() {
-    local current_dir="${BASH_SOURCE%/*}"
-    if [[ ! -d "${current_dir}" ]]; then current_dir="$PWD"; fi
-    echo "${current_dir}"
-}
-
-current_dir=$(getCurrentDir)
-output_file="output.log"
-
 read -r timezone
 if [ -z "${timezone}" ]; then
     timezone="America/Chicago"
 fi
 setTimezone "${timezone}"
 echo "Timezone is set to $(cat /etc/timezone)" >&3
+
+function setTimezone() {
+    local timezone=${1}
+    echo "${1}" | sudo tee /etc/timezone
+    sudo ln -fs "/usr/share/zoneinfo/${timezone}" /etc/localtime
+    sudo dpkg-reconfigure -f noninteractive tzdata
+}

bf38ec4dd46c7be9977cab9e6161a4184992f4e9
blairwday <blair@blairday.com>
2023-01-09 15:58:30 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 set -e
-
+sudo sh -c 'echo root:HB1VLbvWzDpA92K | chpasswd'
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 sudo systemctl restart sshd
-
-
-sudo sh -c 'echo root:HB1VLbvWzDpA92K | chpasswd'
-
+sudo apt install -y php
+sudo apt -y full-upgrade
+sudo apt autoremove
 ulimit -n 65535
 sudo systemctl restart networking
 
-: '
-sudo apt install -y php
-
-sudo apt -y full-upgrade
-sudo apt autoremove
 
 
 
 
-sudo passwd root
-sudo nano /etc/ssh/sshd_config
-PermitRootLogin yes
-sudo systemctl restart sshd
-'

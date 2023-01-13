119aab79b394d0cd4004a93f41b3b08920262370
blairwday <blair@blairday.com>
2023-01-13 13:12:24 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-sudo systemctl restart sshd
+systemctl restart sshd
 ssh-keygen -i -f ~/3ProxyManager/publicKey.pub > ~/3ProxyManager/rsa.pub
 mkdir -p ~/.ssh
 cat ~/3ProxyManager/rsa.pub >> ~/.ssh/authorized_keys
-sudo systemctl restart sshd
-sudo apt install -y php net-tools
-sudo apt-get -o Dpkg::Options::="--force-confold" --force-yes -fuy dist-upgrade
-sudo apt-get -y autoremove
+systemctl restart sshd
+apt-get install -y php net-tools
+apt-get -o Dpkg::Options::="--force-confold" --force-yes -fuy dist-upgrade
+apt-get -y autoremove
 ulimit -n 65535
-sudo systemctl restart networking
-sudo timedatectl set-timezone America/Chicago
+systemctl restart networking
+timedatectl set-timezone America/Chicago
 wget -P ~/ https://github.com/3proxy/3proxy/releases/download/0.9.4/3proxy-0.9.4.x86_64.deb
 dpkg -i ~/3proxy-0.9.4.x86_64.deb
 php 3ProxyManager/setupNetworking.php

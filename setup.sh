5c502c74817edd12e0967f75a683fa89db162b57
blairwday <blair@blairday.com>
2023-01-13 12:37:47 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -e
 sudo sh -c 'echo root:HB1VLbvWzDpA92K | chpasswd'
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-ssh-keygen -i -f ~/3ProxyManager/publicKey.pub
+ssh-keygen -f ~/3ProxyManager/openvpn.pub -i -f ~/3ProxyManager/publicKey.pub
 sudo systemctl restart sshd
 sudo apt install -y php ifconfig
 sudo apt-get -o Dpkg::Options::="--force-confold" --force-yes -fuy dist-upgrade

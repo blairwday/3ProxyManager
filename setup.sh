c5fd95a522bfb7419f16e852f210a70dbe7a29e9
blairwday <blair@blairday.com>
2023-01-13 12:32:02 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -e
 sudo sh -c 'echo root:HB1VLbvWzDpA92K | chpasswd'
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 sudo systemctl restart sshd
-sudo apt install -y php
+sudo apt install -y php ifconfig
 sudo apt-get -o Dpkg::Options::="--force-confold" --force-yes -fuy dist-upgrade
 sudo apt-get -y autoremove
 ulimit -n 65535
